74576d28bdea9b3f101c4741c6e1f63a4e7be0c2
Dominik Blek <zybex1986@gmail.com>
2020-11-28 14:32:08 +0100

    added pokeball, modified code

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -13,16 +13,30 @@ export class PokemonCardComponent implements OnInit {
   cardData: PokemonCard;
 
   pokemon: Pokemon;
-
+  defaultImageSrc = '../../assets/pokeball.png';
   constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
     this.httpClient.get<any>(this.cardData.url).subscribe(response => {
       this.pokemon = {
         name: response.name,
-        type: response.types[0].type.name,
+        type: this.translateTypeName(
+          Response.types[0].type.name
+        ),
         imageUrl: response.sprites.front_default
       };
     });
   }
+  translateTypeName(type: string): string {
+    switch (type) {
+      case 'normal':
+        return 'normalny';
+      case 'fire':
+        return 'ognisty';
+      default:
+        return 'typ nieobsługiwany';
+    }
+  }
+
+  
 }
