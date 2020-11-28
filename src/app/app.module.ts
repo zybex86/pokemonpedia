7887c171ac5ea0891d8273038978023d4f000611
Dominik Blek <zybex1986@gmail.com>
2020-11-28 23:12:28 +0100

    added search bar and improved styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonCardComponent } from './pokemon-card/pokemon-card.component';
 import { PokemonDetailsCardComponent } from './pokemon-details-card/pokemon-details-card.component';
+import { PokemonSearchFormComponent } from './pokemon-search-form/pokemon-search-form.component';
 
 
 @NgModule({
@@ -17,16 +23,22 @@ import { PokemonDetailsCardComponent } from './pokemon-details-card/pokemon-deta
     AppComponent,
     PokemonListComponent,
     PokemonCardComponent,
-    PokemonDetailsCardComponent
+    PokemonDetailsCardComponent,
+    PokemonSearchFormComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatCardModule,
+    BrowserModule,
+    FormsModule,
     HttpClientModule,
-    MatPaginatorModule
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
